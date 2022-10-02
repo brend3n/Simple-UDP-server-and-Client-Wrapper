90727414030658e5c53b373d88127568ae917dda
brend3n <brenden@knights.ucf.edu>
2022-10-01 21:10:38 -0400

    Added more

--- client/client.c
+++ client/client.c
@@ -8,16 +8,21 @@
 #define server_PORT 9090
 // #define server_IP_ADDR "127.0.0.1"
 #define server_IP_ADDR "192.168.1.223"
+char server_message[100], client_message[100];
+
+void clear_buffers(){
+    // Clean buffers:
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));
+}
 
 int main(void){
     int socket_desc;
     struct sockaddr_in server_addr;
-    char server_message[100], client_message[100];
     int server_struct_length = sizeof(server_addr);
     
     // Clean buffers:
-    memset(server_message, '\0', sizeof(server_message));
-    memset(client_message, '\0', sizeof(client_message));
+    clear_buffers();
     
     // Create socket:
     socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -34,6 +39,7 @@ int main(void){
     server_addr.sin_addr.s_addr = inet_addr(server_IP_ADDR);
     
     while (1){
+        clear_buffers();
         // Get input from the user:
         printf("Enter message: ");
         scanf("%s",client_message);
@@ -42,14 +48,14 @@ int main(void){
         if(sendto(socket_desc, client_message, strlen(client_message), 0,
             (struct sockaddr*)&server_addr, server_struct_length) < 0){
             printf("Unable to send message\n");
-            return -1;
+            continue;
         }
         
         // Receive the server's response:
         if(recvfrom(socket_desc, server_message, sizeof(server_message), 0,
             (struct sockaddr*)&server_addr, &server_struct_length) < 0){
             printf("Error while receiving server's msg\n");
-            return -1;
+            continue;            
         }
         
         printf("Server's response: %s\n", server_message);
